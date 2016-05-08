ff58b6e2073229f66b30a5ab160e0b08b0cc7bb2
16.412 Student <16.412 Student>
2016-05-08 15:18:20 -0400

    the start of mdp to reachable boolean

---
+++ mdp_to_reachable.py
@@ -0,0 +1,23 @@
+import sys
+import random
+import mdp
+
+# Takes in an mpd, returns if terminal is reachable
+# from init without hitting something along the way
+def get_reachable(mdp):
+    reachable_states = [mdp.init]
+    new_rs = []
+    policy = policy_iteration(mdp)
+    while True:
+        for state in reachable_states:
+            if state in policy:
+                for (result_state, prob) in mdp.T(state, policy[state]).iteritems():
+                    new_rs = new_rs + [result_state, state]
+                #new_rs = new_rs + [policy[state]]
+        new_rs = list(set(new_rs))
+        if new_rs == reachable_states:
+            print "returning: " + str(new_rs)
+            return new_rs
+        else:
+            reachable_states = new_rs
+    return terminal in reachable_states ## this probably doesn't work yet
